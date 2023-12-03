2ad4c12ba308cfe21613bae2b5a3e7557538d87c
samadhigunasinghe <69573759+samadhi1311@users.noreply.github.com>
2023-12-03 22:07:53 +0530

    Added Boxicons

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
