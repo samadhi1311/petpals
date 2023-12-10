c522b747fddba8a4b6ab1056ffe773dd0cc42079
samadhigunasinghe <samadhilakmina.sl@gmail.com>
2023-12-10 22:35:22 +0530

    Added gradient background

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -3,13 +3,14 @@ import Navigation from '../../global/components/Navigation/Navigation';
 export default function Discover() {
     return (
         <>
-        <header>
+            <div className="gradient-background"></div>
+            <header>
                 <Navigation />
-        </header>
-        <main>
-            <h1>Discover</h1>
-            TODO: Kamshajini: design the Discover Page
-        </main>
+            </header>
+            <main>
+                <h1>Discover</h1>
+                TODO: Kamshajini: design the Discover Page
+            </main>
         </>
     );
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,15 @@
+import Navigation from "../../global/components/Navigation/Navigation";
 export default function SignUp() {
     return (
-        <section>
-            <h1>SignUp</h1>
-            TODO: Kajaani: design the SignUp Page
-        </section>
+        <>
+            <div className="gradient-background"></div>
+            <header>
+                <Navigation />
+            </header>
+            <section>
+                <h1>SignUp</h1>
+                TODO: Kajaani: design the SignUp Page
+            </section>
+        </>
     );
 }
