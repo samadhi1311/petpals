d139aa428110ca2e7a7e2fd73ba9581b58aa3ecf
samadhigunasinghe <69573759+samadhi1311@users.noreply.github.com>
2023-12-13 22:14:56 +0530

    Revert "Revert "Updated navigation bar""
    This reverts commit 09680012876a8737ec911d4351e2351f89b6a690.

--- src/global/components/Navigation/Navigation.jsx
+++ src/global/components/Navigation/Navigation.jsx
@@ -1,57 +1,51 @@
-import { Link, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from '../../assets/logo.svg';
+import '../../styles/global.css';
 import './Navigation.css';
 
-
-// Inside your Navigation component
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 
 export default function Navigation() {
-    // State to track the visibility of the dropdown menu
     const [showMenu, setShowMenu] = useState(false);
     const [clickCount, setClickCount] = useState(0);
 
-
-    // Function to toggle the visibility of the dropdown menu
     const handleMenuClick = () => {
         setShowMenu(!showMenu);
         setClickCount((prevClickCount) => prevClickCount + 1);
     };
 
     return (
-        <html>
-            <head>
-                <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-
-            </head>
+        <>
             <nav>
-                {/* Logo and link to the home page */}
                 <Link to="/" className="logo">
                     <img src={Logo} alt='PetPals' />
                     PetPals
                 </Link>
 
-                {/* Menu icon using box-icon */}
-                <div
-                    className={`menu-icon ${clickCount % 2 === 0 ? "spin" : "anticlockwise"}`}
-                    onClick={handleMenuClick}
-                >
-                    <i className='bx bx-menu'></i>
-                </div>
+                <input type="checkbox" id="nav" className="hidden" />
+                <label htmlFor="nav" className={`nav-btn ${clickCount % 2 === 0 ? "" : "open"}`} onClick={handleMenuClick}>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </label>
 
-                {/* Dropdown menu */}
-                <div className={`nav-links ${showMenu ? "show" : ""}`}>
-                    <Link to="/add">Add</Link>
-                    <Link to="/discover">Discover</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/login">
-                        <button>
-                            <i className='bx bxs-log-in-circle bx-sm' ></i>
-                            Login
-                        </button>
-                    </Link>
+                <div className={`nav-wrapper ${showMenu ? "show" : ""}`}>
+                    <ul>
+                        <li><Link to="/add">Add</Link></li>
+                        <li><Link to="/discover">Discover</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li>
+                            <Link to="/login">
+                                <button>
+                                    <i className='bx bxs-log-in-circle bx-sm' ></i>
+                                    Login
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
-        </html>
+        </>
     );
 }
