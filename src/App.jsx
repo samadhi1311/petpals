4bb25f5d7c19428c9cbe88dffbd1a35a59ede7d1
samadhigunasinghe <87089374+dineth88@users.noreply.github.com>
2023-12-13 13:56:23 +0530

    Added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Discover from './pages/Discover/Discover';
 import Add from './pages/Add/Add';
+import Blog from './pages/Blog/Blog';
+import Discover from './pages/Discover/Discover';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
 
@@ -12,10 +14,12 @@ function App() {
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/add" element={<Add />}></Route>
-        <Route path="/discover" element={<Discover />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/add" element={<Add />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/discover" element={<Discover />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   )
