d3fde3159698dc536cbad8e033788f89d39274eb
samadhigunasinghe <69573759+samadhi1311@users.noreply.github.com>
2023-12-15 12:58:35 +0530

    Added animations to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,67 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 import Navigation from '../../global/components/Navigation/Navigation';
 import './Home.css'
 import HeroImage from '../../global/assets/Adopt a pet-bro.svg';
 
+
 export default function Home() {
+
+    const controlsDiscover = useAnimation();
+    const [refDiscover, inViewDiscover] = useInView({
+        triggerOnce: true,
+        threshold: 0.6, // Adjust this value as needed (0.5 means 50% in view)
+    });
+
+    const controlsAdd = useAnimation();
+    const [refAdd, inViewAdd] = useInView({
+        triggerOnce: true,
+        threshold: 0.6,
+    });
+
+    const controlsBlog = useAnimation();
+    const [refBlog, inViewBlog] = useInView({
+        triggerOnce: true,
+        threshold: 0.6,
+    });
+
+    const controlsAbout = useAnimation();
+    const [refAbout, inViewAbout] = useInView({
+        triggerOnce: true,
+        threshold: 0.6,
+    });
+
+    // Repeat this pattern for other sections as needed
+
+    useEffect(() => {
+        if (inViewDiscover) {
+            controlsDiscover.start({ opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.5 } });
+        }
+    }, [controlsDiscover, inViewDiscover]);
+
+    useEffect(() => {
+        if (inViewAdd) {
+            controlsAdd.start({ opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.5 } });
+        }
+    }, [controlsAdd, inViewAdd]);
+
+    useEffect(() => {
+        if (inViewBlog) {
+            controlsBlog.start({ opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.5 } });
+        }
+    }, [controlsBlog, inViewBlog]);
+
+    useEffect(() => {
+        if (inViewAbout) {
+            controlsAbout.start({ opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.5 } });
+        }
+    }, [controlsAbout, inViewAbout]);
+
+    // Repeat this pattern for other sections as needed
+
     return (
         <>
-
             <div className="gradient-background"></div>
             <header>
                 <Navigation />
@@ -28,38 +84,54 @@ export default function Home() {
                 </section>
             </main>
 
-            <section className='sub-section discover-section'>
-                <div>
-                    <h1>Discover Section</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
-                    <button>Learn More</button>
-                </div>
+            <section ref={refDiscover} className='sub-section discover-section'>
+                <motion.div
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={controlsDiscover}
+                >
+                    <div>
+                        <h1>Discover Section</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
+                        <button>Learn More</button>
+                    </div>
+                </motion.div>
             </section>
 
-            <div className="empty"></div>
-
-            <section className='sub-section add-section'>
-                <div>
-                    <h1>Add Section</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
-                    <button>Learn More</button>
-                </div>
+            <section ref={refAdd} className='sub-section add-section'>
+                <motion.div
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={controlsAdd}
+                >
+                    <div>
+                        <h1>Add Section</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
+                        <button>Learn More</button>
+                    </div>
+                </motion.div>
             </section>
 
-            <section className='sub-section blog-section'>
-                <div>
-                    <h1>Blog Section</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
-                    <button>Learn More</button>
-                </div>
+            <section ref={refBlog} className='sub-section blog-section'>
+                <motion.div
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={controlsBlog}
+                >
+                    <div>
+                        <h1>Blog Section</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
+                        <button>Learn More</button>
+                    </div>
+                </motion.div>
             </section>
 
-            <section className='sub-section about-section'>
-                <div>
-                    <h1>About Section</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
-                    <button>Learn More</button>
-                </div>
+            <section ref={refAbout} className='sub-section about-section'>
+                <motion.div initial={{ opacity: 0, x: 50 }}
+                    animate={controlsAbout}>
+                    <div>
+                        <h1>About Section</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, suscipit quasi possimus facere officiis dicta laborum accusantium vero dolorum quaerat magni, mollitia optio aliquid voluptate. Eligendi explicabo animi nemo quia!</p>
+                        <button>Learn More</button>
+                    </div>
+                </motion.div>
             </section>
 
             <footer>
