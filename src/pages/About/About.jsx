d61b4958a06d8d2920a18e9f0e1be1db9fe22251
dineth88 <87089374+dineth88@users.noreply.github.com>
2023-12-19 04:50:56 +0530

    About container 1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,7 @@
 import Navigation from '../../global/components/Navigation/Navigation';
+import stiker from '../../global/assets/adoption-giving-up-a-catsmall-removebg-preview.png';
+import './About.css';
+
 
 export default function About() {
     return (
@@ -9,8 +12,24 @@ export default function About() {
             </header>
 
             <main>
-                <h1>FAQ and About</h1>
-                TODO: Aashiq - design the About Page
+
+                {/* Two divs side by side */}
+                <div className="content-row">
+                    {/* Left div with text */}
+                    <div className="text-content">
+                        <p style={{ fontSize: '24px' }}>
+                            We're On a Mission To Support And Empower People To Responsibly Rehome Their Pets.
+                        </p>
+                        <p>
+                            Who we are, and why we created PetRehomer: PetRehomer has been set up by a group of animal lovers who are committed to ending pet homelessness and irresponsible rehoming practices. We're part of a registered UK charity, so you can rest assured that we always prioritize pet welfare over profit.
+                        </p>
+                    </div>
+
+                    {/* Right div with image */}
+                    <div className="image-content">
+                        <img src={stiker} alt="Adoption Image" style={{ width: '400px', height: '350px' }}/>
+                    </div>
+                </div>
             </main>
         </>
     );
