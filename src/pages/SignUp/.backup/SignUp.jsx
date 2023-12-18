a2803383352d8513846e62070c3395e36817a0f7
samadhi1311 <69573759+samadhi1311@users.noreply.github.com>
2023-12-18 11:34:59 +0530

    Updated homepage

---
+++ src/pages/SignUp/.backup/SignUp.jsx
@@ -0,0 +1,15 @@
+import Navigation from "../../global/components/Navigation/Navigation";
+export default function SignUp() {
+    return (
+        <>
+            <div className="gradient-background"></div>
+            <header>
+                <Navigation />
+            </header>
+            <section>
+                <h1>SignUp</h1>
+                TODO: Kajaani: design the SignUp Page
+            </section>
+        </>
+    );
+}
