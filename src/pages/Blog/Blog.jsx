ef12bc6ad686f13dd76a09d77c5c766b009969f2
dineth88 <87089374+dineth88@users.noreply.github.com>
2024-02-17 15:46:16 +0530

    Blog Title Div

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,28 @@
+import './Blog.css';
+
 export default function Blog() {
     return (
-
         <main>
             <h1>Blog</h1>
-            TODO: Simar: design the Blog Page
+            <div className="blog-container">
+                <div className="content-text">
+                    <h1 className='content-heading'>
+                        Pet Welfare<br/> Guides
+                    </h1>
+                    <div className="form-row">
+                        {/* First form container */}
+                        <div className="form-container">
+                            {/* Content for the first form container */}
+                            <p>Form content 1</p>
+                        </div>
+                        {/* Second form container */}
+                        <div className="form-container">
+                            {/* Content for the second form container */}
+                            <p>Form content 2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
-
     );
 }
