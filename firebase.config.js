9b698bac77624d6e6cfb978348f53ed158219a4c
Kamsha-ravi <147122436+Kamsha-ravi@users.noreply.github.com>
2024-02-20 11:49:07 +0530

    Added post addition functionality

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const googleAuthProvider = new GoogleAuthProvider();
