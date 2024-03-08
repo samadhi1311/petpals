82054a7d1bd648065648aa7cabad8995fe32b626
MohamedSimar <gassalimohamedsimar@gmail.com>
2024-03-08 11:07:39 +0530

    Updated URLs

--- src/global/components/Navigation/Navigation.jsx
+++ src/global/components/Navigation/Navigation.jsx
@@ -1,73 +1,89 @@
-import { Link, useNavigate } from "react-router-dom";
-import { auth } from "../../../../firebase.config";
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../../../../firebase.config';
 import 'boxicons/css/boxicons.min.css';
 import Logo from '../../assets/logo.svg';
 import '../../styles/global.css';
 import './Navigation.css';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export default function Navigation() {
-    const [showMenu, setShowMenu] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(!!auth.currentUser);
+	const [showMenu, setShowMenu] = useState();
+	const [isLoggedIn, setIsLoggedIn] = useState(!!auth.currentUser);
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setIsLoggedIn(!!user);
-        });
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setIsLoggedIn(!!user);
+		});
 
-        return () => unsubscribe();
-    }, []);
+		return () => unsubscribe();
+	}, []);
 
-    const handleMenuClick = () => {
-        setShowMenu((prevShowMenu) => !prevShowMenu);
-    };
+	const handleMenuClick = () => {
+		setShowMenu((prevShowMenu) => !prevShowMenu);
+	};
 
-    const handleLinkClick = () => {
-        document.getElementById("nav").click(); // Trigger a click on the checkbox with id "nav"
-    };
+	const handleLinkClick = () => {
+		document.getElementById('nav').click(); // Trigger a click on the checkbox with id "nav"
+	};
 
-    return (
-        <>
-            <nav>
-                <Link to="/" className="logo">
-                    <img src={Logo} className="nav-logo" alt='PetPals' />
-                    PetPals
-                </Link>
+	return (
+		<>
+			<nav>
+				<Link to='/PetPals' className='logo'>
+					<img src={Logo} className='nav-logo' alt='PetPals' />
+					PetPals
+				</Link>
 
-                <input type="checkbox" id="nav" className="hidden" onChange={handleMenuClick} />
-                <label htmlFor="nav" className={`nav-btn ${showMenu ? "open" : ""}`}>
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                </label>
+				<input type='checkbox' id='nav' className='hidden' onChange={handleMenuClick} />
+				<label htmlFor='nav' className={`nav-btn ${showMenu ? 'open' : ''}`}>
+					<i></i>
+					<i></i>
+					<i></i>
+				</label>
 
-                <div className={`nav-wrapper ${showMenu ? "show" : ""}`}>
-                    <ul>
-                        <li><Link to="/add" onClick={handleLinkClick}>Add</Link></li>
-                        <li><Link to="/discover" onClick={handleLinkClick}>Discover</Link></li>
-                        <li><Link to="/blog" onClick={handleLinkClick}>Blog</Link></li>
-                        <li><Link to="/about" onClick={handleLinkClick}>About</Link></li>
-                        <li>
-                            {isLoggedIn ? (
-                                <Link to="/user/me" onClick={handleLinkClick}>
-                                    <button>
-                                        <i className='bx bxs-cat bx-sm'></i>
-                                        {auth.currentUser.displayName || auth.currentUser.email}
-                                    </button>
-                                </Link>
-                            ) : (
-                                <Link to="/login" onClick={handleLinkClick}>
-                                    <button>
-                                        <i className='bx bxs-log-in-circle bx-sm'></i>
-                                        Login
-                                    </button>
-                                </Link>
-                            )}
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    );
+				<div className={`nav-wrapper ${showMenu ? 'show' : ''}`}>
+					<ul>
+						<li>
+							<Link to='PetPals/add' onClick={handleLinkClick}>
+								Add
+							</Link>
+						</li>
+						<li>
+							<Link to='PetPals/discover' onClick={handleLinkClick}>
+								Discover
+							</Link>
+						</li>
+						<li>
+							<Link to='PetPals/blog' onClick={handleLinkClick}>
+								Blog
+							</Link>
+						</li>
+						<li>
+							<Link to='PetPals/about' onClick={handleLinkClick}>
+								About
+							</Link>
+						</li>
+						<li>
+							{isLoggedIn ? (
+								<Link to='PetPals/users/me' onClick={handleLinkClick}>
+									<button>
+										<i className='bx bxs-cat bx-sm'></i>
+										{auth.currentUser.displayName || auth.currentUser.email}
+									</button>
+								</Link>
+							) : (
+								<Link to='PetPals/login' onClick={handleLinkClick}>
+									<button>
+										<i className='bx bxs-log-in-circle bx-sm'></i>
+										Login
+									</button>
+								</Link>
+							)}
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
 }
